fbb45662d16f91bb50602187b09d2398bd71a5ae
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-10-02 08:19:05 -0700

    Update build.sh
    Replit

--- build.sh
+++ build.sh
@@ -1,39 +1,50 @@
+
 #!/bin/sh
-
-
 rm 
+
 -rf pkg
-wasm-pack build --target=web || exit 1
-
-# Find value of argument --manifest-version
-for arg in "$@"; do
-  case "$arg" in
-    --manifest-version=*)
-      manifest_version="${arg#*=}"
-      ;;
-  esac
-done
-
-# Copy manifest.json to pkg
-if 
-[ "$manifest_version" = "v3" ]
+-wasm-pack 
+-build 
+--target=web
 || 
-[ "$manifest_version" = "3" ]; then
-  cp manifest_v3.json pkg/manifest.json
-else if [ "$manifest_version" = "v2" ] || [ "$manifest_version" = "2" ]; then
-  cp manifest_v2.json pkg/manifest.json
+-exit 1
+
+#!
+-Find value of argument 
+--manifest-version
+--for arg in 
+"$@"; do
+  case "$arg" in
+      --manifest_version=*"${arg#*=}"
+      ;;esac
+   --done
+#!
+-Copy 
+--manifest.json to pkg
+'if 
+["$manifest_version" = "v3" ]
+|| 
+["$manifest_version" = "3" ]; then
+  --cp manifest_v3.json pkg/manifest.json
+else if
+["$manifest_version" = "v2" ] 
+|| 
+[ "$manifest_version" = "2" ]; then
+  --cp manifest_v2.json pkg/manifest.json
 else
-  echo "Packaging with manifest version v2. Pass --manifest-version=v3 to package with manifest version 3."
+  $echo 
+  "Packaging with manifest version v2. 
+  Pass
+  --manifest-version=v3 to package with manifest version 3."
   cp manifest_v2.json pkg/manifest.json
   fi
-# Copy test.html to pkg
-cp test.html pkg/test.html
+#! 
+--Copy test.html to pkg
+--cp test.html pkg/test.html
 printf "
-const runtime = chrome.runtime || browser.runtime;
-
-async function run() {
-  await wasm_bindgen(runtime.getURL('vail_verifier_extension_bg.wasm'));
-}
-
-run();
-" >> pkg/run_wasm.js
+$const
+runtime = chrome.runtime 
+||
+browser.runtime; async function run()
+{ await wasm_bindgen(runtime.getURL('vail_verifier_extension_bg.wasm'));
+}run();" >> pkg/run_wasm.js
